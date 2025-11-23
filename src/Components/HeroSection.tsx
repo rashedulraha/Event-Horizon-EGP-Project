d036e851032efef71c79e242e06425179fbbb461
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-11-23 19:06:05 +0600

    impliment hero sectiong

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -2,9 +2,40 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="container mx-auto px-4">
-      <h1>Hero section</h1>
-    </div>
+    <section className="bg-black text-white py-24">
+      <div className="container mx-auto px-6 text-center max-w-4xl">
+        <h1 className="text-5xl md:text-6xl font-extrabold leading-tight">
+          Discover Events That <br /> Defy Expectations
+        </h1>
+
+        <p className="text-gray-300 mt-6 text-lg md:text-xl">
+          Join the world&apos;s most innovative event platform. Find tech
+          conferences, music festivals, and networking meetups tailored just for
+          you.
+        </p>
+
+        <div className="mt-10 flex items-center justify-center gap-4">
+          <button className="bg-white text-black font-medium px-6 py-2 rounded-md hover:opacity-80 transition cursor-pointer">
+            Explore Events →
+          </button>
+
+          <button className="border border-gray-700 px-6 py-2 rounded-md hover:bg-gray-800 transition cursor-pointer">
+            Create Account
+          </button>
+        </div>
+
+        <div className="my-10 max-w-4xl flex items-center justify-between ">
+          <div>
+            <h2
+              className="font-bold text-white text-2
+              ">
+              50K+
+            </h2>
+            <span className="uppercase text-white/30">active user</span>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
